d0fa4979f101b8a2cebdf9a7c9d12c892a50c4d1
Belreau Cohen <nuu.chah.nulth@disroot.org>
2022-03-12 18:36:23 +0700

    soundex_hash: шаг #4

--- src/libcsc/libcsc/soundex/soundex.cpp
+++ src/libcsc/libcsc/soundex/soundex.cpp
@@ -12,7 +12,7 @@ uint32_t string_to_hash(const std::string& str)
     return std::accumulate(str.begin(), str.begin() + 4, 0, [](uint32_t&& hash, char&& c) { return (hash << 8) + c; });
 }
 
-void delete_chars(std::string::iterator& begin, std::string::iterator& end, std::initializer_list<char>&& chars_to_delete)
+void delete_chars(std::string::iterator& begin, std::string::iterator& end, const std::initializer_list<char>& chars_to_delete)
 {
     for (const char& char_to_delete : chars_to_delete) {
         std::remove(begin, end, char_to_delete);
@@ -26,6 +26,11 @@ void replace_chars(std::string::iterator& begin, std::string::iterator& end, con
     }
 }
 
+void clear_adjacent_numbers(std::string::iterator& begin, std::string::iterator& end)
+{
+    std::unique(begin, end, [](const char& c1, const char& c2) { return (c1 == c2) && (std::isdigit(c1)); });
+}
+
 uint32_t soundex_hash(std::string_view&& str)
 {
     std::string hashbase(str);
@@ -36,6 +41,8 @@ uint32_t soundex_hash(std::string_view&& str)
     for (const auto& [char_for_replace, chars_to_replace] : values_to_key_map) {
         replace_chars(hashbase.begin(), hashbase.end(), chars_to_replace, char_for_replace);
     }
+
+    clear_adjacent_numbers(hashbase.begin(), hashbase.end());
 
     delete_chars(std::next(hashbase.begin()), hashbase.end(), {'a', 'e', 'i', 'o', 'u', 'y'});
     hashbase.resize(4, '0');
