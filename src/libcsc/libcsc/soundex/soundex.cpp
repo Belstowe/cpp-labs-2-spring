f96b9a4b1b7cccc77c0e59d6b95eeb6f24729c1c
Belreau Cohen <nuu.chah.nulth@disroot.org>
2022-03-31 12:10:27 +0700

    soundex: примитивы, string_view: by ref => by val

--- src/libcsc/libcsc/soundex/soundex.cpp
+++ src/libcsc/libcsc/soundex/soundex.cpp
@@ -9,10 +9,10 @@
 namespace libcsc::soundex {
 uint32_t string_to_hash(const std::string& str)
 {
-    return std::accumulate(str.rbegin(), str.rend(), 0, [](uint32_t hash, const char& c) { return (hash << 8) + c; });
+    return std::accumulate(str.rbegin(), str.rend(), 0, [](uint32_t hash, const char c) { return (hash << 8) + c; });
 }
 
-std::string hash_to_string(const uint32_t& hash)
+std::string hash_to_string(const uint32_t hash)
 {
     return std::string(reinterpret_cast<const char*>(&hash), 4);
 }
@@ -20,39 +20,39 @@ std::string hash_to_string(const uint32_t& hash)
 void delete_chars(std::string& str, const std::string::iterator& begin, const std::string::iterator& end, const std::initializer_list<char>& chars_to_delete)
 {
     auto new_end = std::remove_if(
-            begin, end, [chars_to_delete](const char& c) { return (std::find(chars_to_delete.begin(), chars_to_delete.end(), c) != chars_to_delete.end()); });
+            begin, end, [chars_to_delete](const char c) { return (std::find(chars_to_delete.begin(), chars_to_delete.end(), c) != chars_to_delete.end()); });
     str.erase(new_end, end);
 }
 
 void replace_chars(
-        const std::string::iterator& begin, const std::string::iterator& end, const std::initializer_list<char>& chars_to_replace, const char& char_for_replace)
+        const std::string::iterator& begin, const std::string::iterator& end, const std::initializer_list<char>& chars_to_replace, const char char_for_replace)
 {
     std::replace_if(
             begin,
             end,
-            [chars_to_replace](const char& c) { return (std::find(chars_to_replace.begin(), chars_to_replace.end(), c) != chars_to_replace.end()); },
+            [chars_to_replace](const char c) { return (std::find(chars_to_replace.begin(), chars_to_replace.end(), c) != chars_to_replace.end()); },
             char_for_replace);
 }
 
 void clear_adjacent_numbers(std::string& str, const std::string::iterator& begin, const std::string::iterator& end)
 {
-    auto new_end = std::unique(begin, end, [](const char& c1, const char& c2) { return (c1 == c2) && (std::isdigit(c1)); });
+    auto new_end = std::unique(begin, end, [](const char c1, const char c2) { return (c1 == c2) && (std::isdigit(c1)); });
     str.erase(new_end, end);
 }
 
-uint32_t soundex_hash(const std::string_view& str)
+uint32_t soundex_hash(const std::string_view str)
 {
     if (str.empty()) {
         throw std::runtime_error("Empty string given to soundex_hash");
     }
-    std::for_each(str.begin(), str.end(), [str](const char& c) {
+    std::for_each(str.begin(), str.end(), [str](const char c) {
         if (!isdigit(static_cast<int>(c)) && !isalpha(static_cast<int>(c))) {
             throw std::runtime_error(std::string() + "String '" + str.data() + "' includes a character which is neither a letter nor a digit");
         }
     });
 
     std::string hashbase(str);
-    std::transform(hashbase.begin(), hashbase.end(), hashbase.begin(), [](char& c) -> char { return static_cast<char>(std::tolower(c)); });
+    std::transform(hashbase.begin(), hashbase.end(), hashbase.begin(), [](const char c) -> char { return static_cast<char>(std::tolower(c)); });
 
     delete_chars(hashbase, std::next(hashbase.begin()), hashbase.end(), {'h', 'w'});
 
