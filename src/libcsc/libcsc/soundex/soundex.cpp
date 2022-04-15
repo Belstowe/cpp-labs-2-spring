412db5bcf978bce4bf6ca823823c6d255d7586fe
Belreau Cohen <nuu.chah.nulth@disroot.org>
2022-04-15 22:09:45 +0700

    soundex: оптимизирован replace_chars

--- src/libcsc/libcsc/soundex/soundex.cpp
+++ src/libcsc/libcsc/soundex/soundex.cpp
@@ -30,18 +30,15 @@ void delete_chars(
 }
 
 void replace_chars(
-        const std::string::iterator& begin,
+        std::string::iterator begin,
         const std::string::iterator& end,
-        const std::initializer_list<char>& chars_to_replace,
-        const char char_for_replace)
+        const std::unordered_map<char, char>& replace_map)
 {
-    std::replace_if(
-            begin,
-            end,
-            [chars_to_replace](const char c) {
-                return (std::find(chars_to_replace.begin(), chars_to_replace.end(), c) != chars_to_replace.end());
-            },
-            char_for_replace);
+    for (; begin != end; begin++) {
+        if (replace_map.find(*begin) != replace_map.cend()) {
+            *begin = replace_map.at(*begin);
+        }
+    }
 }
 
 void clear_adjacent_numbers(std::string& str, const std::string::iterator& begin, const std::string::iterator& end)
@@ -49,6 +46,18 @@ void clear_adjacent_numbers(std::string& str, const std::string::iterator& begin
     auto new_end
             = std::unique(begin, end, [](const char c1, const char c2) { return (c1 == c2) && (std::isdigit(c1)); });
     str.erase(new_end, end);
+}
+
+const std::unordered_map<char, char>
+reverse_map(std::initializer_list<std::pair<char, std::initializer_list<char>>>&& map_to_reverse)
+{
+    std::unordered_map<char, char> result_map;
+    for (const auto& [key, value_list] : map_to_reverse) {
+        for (auto&& value : value_list) {
+            result_map[value] = key;
+        }
+    }
+    return result_map;
 }
 
 uint32_t soundex_hash(const std::string_view str)
@@ -72,16 +81,14 @@ uint32_t soundex_hash(const std::string_view str)
 
     delete_chars(hashbase, std::next(hashbase.begin()), hashbase.end(), {'h', 'w'});
 
-    const std::unordered_map<char, std::initializer_list<char>> values_to_key_map
-            = {{'1', {'b', 'f', 'p', 'v'}},
-               {'2', {'c', 'g', 'j', 'k', 'q', 's', 'x', 'z'}},
-               {'3', {'d', 't'}},
-               {'4', {'l'}},
-               {'5', {'m', 'n'}},
-               {'6', {'r'}}};
-    for (const auto& [char_for_replace, chars_to_replace] : values_to_key_map) {
-        replace_chars(hashbase.begin(), hashbase.end(), chars_to_replace, char_for_replace);
-    }
+    const std::unordered_map<char, char> values_to_key_map = reverse_map(
+            {{'1', {'b', 'f', 'p', 'v'}},
+             {'2', {'c', 'g', 'j', 'k', 'q', 's', 'x', 'z'}},
+             {'3', {'d', 't'}},
+             {'4', {'l'}},
+             {'5', {'m', 'n'}},
+             {'6', {'r'}}});
+    replace_chars(hashbase.begin(), hashbase.end(), values_to_key_map);
 
     clear_adjacent_numbers(hashbase, hashbase.begin(), hashbase.end());
 
